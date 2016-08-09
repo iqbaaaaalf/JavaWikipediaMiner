f05916d850fef3e83309cea9a117596f65cc2b33
Iqbal Fakhri Hendri <iqbaaaaalf@app.ipb.ac.id>
2016-08-09 11:13:09 +0700

    first commit ever

---
+++ src/Cleaner.java
@@ -0,0 +1,155 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.wikipedia.miner.model.Article;
+import org.wikipedia.miner.model.Page;
+import org.wikipedia.miner.util.MarkupStripper;
+
+import com.google.common.base.CharMatcher;
+
+public class Cleaner {
+	
+	public String getSentenceClean(Page isi) throws Exception{
+		String markupDirt = isi.getMarkup(); 
+		
+		//using guava //
+		//inisiasi char apa yang akan disamakan pada suatu text
+		String charsToRemove = "*!|:";
+		String CharsToRemove1 = "[]'" ; 
+		//mengahapus masing masing symbol dari  charsToRemove pada String markup
+		String markupDirt1 = CharMatcher.anyOf(charsToRemove).replaceFrom(markupDirt, " ");
+		//mengahapus masing masing symbol dari  charsToRemove1 pada String markup
+		String markupClean = CharMatcher.anyOf(CharsToRemove1).replaceFrom(markupDirt1, "");
+		//menghapus tag dan content pada tag <ref> sampai close tag nya
+		//using guava//
+		
+		// menghapus semua gallery tag (atribut) dengan contentnya
+		markupClean = markupClean.replaceAll("(?s)<gallery>.*?</gallery>", "");
+		// menghapus semua ref tag (atribut) dengan contentnya
+		markupClean = markupClean.replaceAll("(?s)<ref\\s(.*?)>(.*?)</ref>", " ");
+		// menghapus semua external link
+		markupClean = markupClean.replaceAll("\\[(http|www)(.*?)\\]", " ");
+		// menghapus semua html tag yang tersisa
+		markupClean = markupClean.replaceAll("<(.*?)>", " ");
+		// menghapus markup untuk bold dan italic
+		markupClean = markupClean.replaceAll("'{2,}", " ");
+		// menghilangkan tab menjorong kedalam
+		markupClean = markupClean.replaceAll("\n:+", " ");
+		// menghapus penanda list
+		markupClean = markupClean.replaceAll("\n([\\*\\#]+)", " ");
+		//1 newline diganti 2
+		//markupClean = markupClean.replaceAll("\\n", "\n\n");
+		// membersihkan tanda punctuation
+		markupClean = markupClean.replaceAll("[\\[\\]\\{\\}]", "");
+		// membersihkan penanda '===' header
+		markupClean = markupClean.replaceAll("===", "");
+		markupClean = markupClean.replaceAll("==", "");
+		markupClean = markupClean.replaceAll("&nbsp;", " ");
+		markupClean.trim();
+		return markupClean ;
+	}
+	
+	public String getBox(Page isi){
+		String markupDirt = isi.getMarkup();
+		String charsToRemove = "*!:|";
+		String CharsToRemove1 = "'" ; 
+		Pattern p = Pattern.compile("(Infobox.*\n(?:|.*\n)+)}}");
+		Matcher m = p.matcher(markupDirt);
+		String markupClean = null;
+		if (m.find())
+		{
+			markupClean = m.group(1);
+			markupClean = CharMatcher.anyOf(charsToRemove).replaceFrom(markupClean, " ");
+			markupClean = CharMatcher.anyOf(CharsToRemove1).replaceFrom(markupClean, "");
+			//menghilangan tag dan isi dalam tag
+			markupClean = markupClean.replaceAll("(?s)<small\\s(.*?)>(.*?)</small>", " ");
+			// menghapus tag <br  />
+			markupClean = markupClean.replaceAll("<br />|<br>", "");
+			// menghapus semua ref tag (atribut) dengan contentnya
+			markupClean = markupClean.replaceAll("(?s)<ref\\s(.*?)>(.*?)</ref>", " ");
+			//menghilangkan tanda < spanberserta stylenya
+			markupClean = markupClean.replaceAll("(?s)<span\\s(.*?)>(.*?)</span>", " $2  ");
+			// menghilangkan tag sub 
+			markupClean = markupClean.replaceAll("(?s)<sub>(.*?)</sub>", "");
+			// menghapus tanda <sup> yang menandakan pangkat
+			markupClean = markupClean.replaceAll("<sup>", "^");
+			// untuk mengganti tanda ?didepan angka yangseharusnya adalah -
+			// mengganti tanda <sup> dengan space
+			markupClean = markupClean.replaceAll("</sup>", " ");
+			//menghapus tanda [ ] ( )
+			markupClean = markupClean.replaceAll("[\\(\\)\\[\\]]", "");
+			// menghapus tanda dan isi diantara bracket (?) mengatasi native title dari 'bahasa' yang tidak dapat esktrak(eg: bahasa indonesia, bahasa jawa)
+			markupClean = markupClean.replaceAll("(\\{\\{.*}})", "");
+			//menambahkan whitespace setelah '='
+			markupClean = markupClean.replaceAll("=(?=.)", "$0 ");
+			// menghilangkan tanda tanya berlebih
+			markupClean = markupClean.replaceAll("[\\?]", "");
+			//menghilangkan tanda &nbsp hasil ekstraksi suatu simbol
+			markupClean = markupClean.replaceAll("&nbsp;", " ");
+			markupClean = markupClean.replaceAll("\\?(\\d)", "-$2");
+			
+			
+		}
+		else
+			markupClean = "";
+		
+//		markupClean = CharMatcher.anyOf(charsToRemove).replaceFrom(markupClean, " ");
+//		markupClean = markupClean.replaceAll("[\\[\\]\\{\\}]", "");
+		//String markupClean = markupDirt;
+		return markupClean;
+	}
+	
+	public String getDesc(Page isi){
+		MarkupStripper stripper = new MarkupStripper() ;
+		String markupDirt = isi.getMarkup();
+		String markupClean = "";
+		
+		markupClean = markupDirt.replaceAll("={2,}(.+)={2,}", "\n") ; //clear section headings completely - not just formating, but content as well.			
+		markupClean = stripper.stripAllButInternalLinksAndEmphasis(markupClean, null) ;
+		markupClean = stripper.stripNonArticleInternalLinks(markupClean, null) ;
+		markupClean = stripper.stripExcessNewlines(markupClean) ;
+
+		String fp = "" ;
+		int pos = markupClean.indexOf("\n\n") ;
+
+		while (pos>=0) {
+			fp = markupClean.substring(0, pos) ;
+
+			if (pos > 100000) 
+				break ;
+
+			pos = markupClean.indexOf("\n\n", pos+2) ;
+		}
+
+		fp = fp.replaceAll("\n", " ") ;
+		fp = fp.replaceAll("\\s+", " ") ;  //turn all whitespace into spaces, and collapse them.
+		fp = fp.trim();
+		
+		
+		return fp;
+	}
+	
+
+	public Boolean isKategori(String kategori, String query){
+		Pattern p = Pattern.compile("(?! )("+query+")(?: )", Pattern.CASE_INSENSITIVE);
+		Matcher m = p.matcher(kategori);
+		
+		if (m.find()){
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public Boolean isKategoriFix(String kategori, String query){
+		Pattern p = Pattern.compile("(\b"+query+"\b)");
+		Matcher m = p.matcher(kategori);
+		
+		if (m.find()){
+			return true;
+		}
+		
+		return false;
+	}
+	
+}
---
+++ src/Wikicorpus.java
@@ -0,0 +1,431 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.* ;
+
+import org.wikipedia.miner.model.Article;
+import org.wikipedia.miner.model.Category;
+import org.wikipedia.miner.model.Page;
+import org.wikipedia.miner.model.Wikipedia;
+import org.wikipedia.miner.model.Page.PageType;
+import org.wikipedia.miner.util.WikipediaConfiguration;
+
+
+
+public class Wikicorpus {
+		
+		static Wikipedia _wikipedia;
+		BufferedWriter out;
+		BufferedReader _input;
+		
+public Wikicorpus(WikipediaConfiguration conf){ 
+		// memuat configurasi wiki, untuk akses ke database barkeley
+		_wikipedia = new Wikipedia(conf, false);
+	}
+	
+/*
+ * @param file adalah sebuah file yang berisi seluruh id artikel, category, redirect, disambigu dari wikipedia
+ * @param wikipedia adalah env wikipedia yang sedang dijalankan  
+ */
+
+public void ArticleSet(File file, Wikipedia wikipedia) throws Exception{
+	
+	BufferedReader reader = new BufferedReader(new FileReader(file)) ;
+	String line  ;
+
+	
+	while ((line = reader.readLine()) != null) {
+		String[] values = line.split("\t") ;
+		int id = new Integer(values[0].trim()) ;
+		Article article = (Article) wikipedia.getPageById(id);
+		displayMarkup(article);
+		
+		}
+	reader.close();
+}
+
+/*
+ * @param isi adalah page yang ingin dibersihkan markupnya dan tambah ke dalam file
+ * 
+ */
+
+private void displayMarkup(Page isi)throws Exception {
+	Cleaner markupclean = new Cleaner();
+	String hasil = markupclean.getSentenceClean(isi);
+	try{
+	out = new BufferedWriter(new FileWriter("T:/Project2/Corpus-FULL.xml",true));
+
+	out.append(hasil);
+	out.newLine();
+	out.newLine();
+    out.close();
+	}catch(IOException e){
+		System.out.println("There was a problem:" + e);
+	}
+}
+
+/*
+ * @param p adalah page yang akan diambil title dan id nya yang kemudian dimasukan kedalam file
+ * 
+ */
+
+protected void deskrip(Page p) throws Exception {
+	String title = p.getTitle();
+	int id = p.getId();
+	
+	try{
+		out = new BufferedWriter(new FileWriter("X:/Database Berkeley/Output/templateById.tsv",true));
+		out.append(String.format("%-15s %-15s %n", id, title));
+		out.close();
+		System.out.println("Artikel dengan " +id+ " telah di deskrip");
+		}catch(IOException e){
+			System.out.println("There was a problem:" + e);
+		}
+}
+
+
+private void deskriptor() throws Exception{
+	Iterator<Page> iter = _wikipedia.getPageIterator(PageType.article); 
+	Page p ;
+	
+	while(iter.hasNext()){
+		Page p1 = iter.next();
+		p=p1;
+		deskrip(p);	
+	}
+}
+
+
+/*
+ * method untuk melakukan iterasi terhadap artikel yang ada kemudian di bersihkan (membuat corpus)
+ */
+
+
+private void corpusClean()  throws Exception {
+	Iterator<Page> iter = _wikipedia.getPageIterator(PageType.article); 
+	Page p ;
+	while(iter.hasNext()){
+		Page p1 = iter.next();
+		p=p1;
+		displayMarkup(p);	
+	}
+}
+
+/*
+ * method bantuan untuk memabersihkan artikel dan mengambil box nya
+ */
+
+private void box(Page p) throws Exception{
+	Cleaner c = new Cleaner();
+	String hasil = c.getBox(p);
+	
+	int id = p.getId();
+	
+	/*
+	 * tidak semua artikel memiliki infobox
+	 */
+	if (hasil == ""){
+		
+		try{
+			out = new BufferedWriter(new FileWriter("X:/Database Berkeley/Output/box/nobox/list.nobox.txt", true));
+			out.append(String.format("%-15s %-15s %n", p.getId(), p.getTitle()));
+			out.newLine();
+			out.close();
+			System.out.println( id+ " tidak mempunyai box");
+		}catch (IOException e){
+			System.out.println("There was a problem:" + e);
+		}
+		
+	}else{
+		
+		try{
+			out = new BufferedWriter(new FileWriter("X:/Database Berkeley/Output/box/"+ id + ".box.txt"));
+			out.write(hasil);
+			out.newLine();
+			out.close();
+			System.out.println("Artikel dengan id " +id+ " telah di diextract boxnya");
+			}catch(IOException e){
+				System.out.println("There was a problem:" + e);
+			}
+	
+	}
+}
+
+
+/*
+ * method untuk mengambil info box dari wikipedia
+ */
+
+private void boxer() throws Exception {
+	Iterator<Page> iter = _wikipedia.getPageIterator(PageType.article); 
+	Page p ;
+	while(iter.hasNext()){
+		Page p1 = iter.next();
+		p=p1;
+		box(p);	
+	}
+}
+
+private void doc(Page p){
+	Cleaner c = new Cleaner();
+	String hasil = c.getDesc(p);
+	
+	System.out.println(hasil);
+	
+}
+
+private void docer() {
+	 Page article = _wikipedia.getPageById(44);
+	 doc(article);
+	 
+//     System.out.println(article.getType());
+//     System.out.println(article.getTitle());
+//     System.out.println(article.getMarkup());
+//     System.out.println(article.getFirstParagraphMarkup());
+	
+}
+
+private String ambilKategori(String ask, String p, int id) throws IOException{
+	Page kategori = _wikipedia.getCategoryByTitle(p);
+	String judul = null;
+	String query = ask;
+	String hasil = null;
+	Cleaner c = new Cleaner();
+	
+	
+	if(kategori != null){
+		judul = kategori.getTitle();
+		if(c.isKategori(judul, query)){
+			hasil = judul;
+		}else
+			hasil = null;
+		}
+	else
+		hasil = null;
+	
+	return hasil;
+}
+
+private String ambilArtikel(String ask, String p, int id) throws IOException{
+	Page kategori = _wikipedia.getCategoryByTitle(p);
+	String judul = null;
+	String query = ask;
+	String hasil = null;
+	Cleaner c = new Cleaner();
+	
+	
+	if(kategori != null){
+		judul = kategori.getTitle();
+		if(c.isKategoriFix(judul, query)){
+			hasil = p;
+		}else
+			hasil = null;
+		}
+	else
+		hasil = null;
+	
+	return hasil;
+}
+
+
+private Integer getInt(String prompt, int min, int max) throws IOException {
+	
+	while (true){
+		
+		System.out.println(prompt + " (" + min + " - " + max + " atau ENTER untuk mengkosongkan)");
+		
+		String line = _input.readLine();
+		if (line.trim().equals(""))
+			return null;
+		
+		try{
+			Integer val = Integer .parseInt(line);
+			if (val >= min && val <= max)
+                return val ;
+		}catch (Exception e){
+			
+		}
+		
+		System.out.println("Input tidak tepat, silakan mencoba lagi");
+				
+	}
+			
+}
+
+private String getString(String prompt) throws IOException{
+	System.out.println(prompt + "(atau ENTER untuk mengosongkan");
+	String line = _input.readLine();
+	if (line.trim().equals(""))
+		return null;
+	return line;
+}
+
+private void getArticleByCategory() throws Exception{
+	Iterator<Page> iter = _wikipedia.getPageIterator(PageType.article);
+	Iterator<Page> iter2 = _wikipedia.getPageIterator(PageType.article);
+	_input = new BufferedReader(new InputStreamReader(System.in));
+	Cleaner c = new Cleaner();
+	Page p;
+	String termKategori;
+	Page penampung;
+	List<String> artikelTerkait = new ArrayList<String>();
+	List<String> kategoriTerkait = new ArrayList<String>(); 
+	
+	while((termKategori = getString("Silakan masukan kategory yang anda mau cari")) != null){
+	
+			while(iter.hasNext()){
+				Page p1 = iter.next();
+				p=p1;
+				System.out.println(p.getId());
+				String isi = ambilKategori(termKategori, p.getTitle(), p.getId());
+				if (isi != null)
+				kategoriTerkait.add(isi);
+			}
+			
+			if(kategoriTerkait.size() == 1){
+				System.out.println(" - [1] " + kategoriTerkait.get(0)) ;
+				Integer kategoriIndex = getInt("Pilih Kategori yang anda cari", 1, kategoriTerkait.size());
+				while(iter2.hasNext()){
+					Page p1 = iter2.next();
+					p=p1;
+					System.out.println(p.getId());
+					String artikel = ambilArtikel(kategoriTerkait.get(kategoriIndex), p.getTitle(), p.getId());
+					if (artikel != null)
+					artikelTerkait.add(artikel);
+				}
+				
+				if (artikelTerkait.size() == 1){
+					penampung = _wikipedia.getArticleByTitle(artikelTerkait.get(1));
+					System.out.println(penampung.getMarkup());
+				}else{
+					System.out.println("== kategori " + termKategori + " ada di dalam beberapa artikel yaitu :");
+					for (int i=0 ; i<artikelTerkait.size() ; i++) {
+			            System.out.println(" - [" + (i+1) + "] " + artikelTerkait.get(i));
+				}
+					Integer artikelIndex = getInt("Pilih artikel yang anda cari", 1, artikelTerkait.size());
+					penampung = _wikipedia.getArticleByTitle(artikelTerkait.get(artikelIndex));
+					System.out.println(penampung.getMarkup());
+				}
+				
+			}else{
+				System.out.println("== term kategori " + termKategori + " ada di dalam beberapa kategori yaitu :");
+				for (int i=0 ; i<kategoriTerkait.size() ; i++) {
+		            System.out.println(" - [" + (i+1) + "] " + kategoriTerkait.get(i));
+		        }	
+				Integer kategoriIndex = getInt("Pilih Kategori yang anda cari", 1, kategoriTerkait.size());
+				while(iter2.hasNext()){
+					Page p1 = iter2.next();
+					p=p1;
+					System.out.println(p.getId());
+					String artikel = ambilArtikel(kategoriTerkait.get(kategoriIndex), p.getTitle(), p.getId());
+					if (artikel != null)
+					artikelTerkait.add(artikel);
+				}
+				
+				if (artikelTerkait.size() == 1){
+					penampung = _wikipedia.getArticleByTitle(artikelTerkait.get(1));
+					System.out.println(penampung.getMarkup());
+				}else{
+					System.out.println("== kategori " + termKategori + " ada di dalam beberapa artikel yaitu :");
+					for (int i=0 ; i<artikelTerkait.size() ; i++) {
+			            System.out.println(" - [" + (i+1) + "] " + artikelTerkait.get(i));
+				}
+					Integer artikelIndex = getInt("Pilih artikel yang anda cari", 1, artikelTerkait.size());
+					penampung = _wikipedia.getArticleByTitle(artikelTerkait.get(artikelIndex));
+					System.out.println(penampung.getMarkup());
+				}
+			}
+	}
+				
+				
+			
+				
+//				
+//				System.out.println("== Kategori " + termKategori + " mencangkup beberapa artikel yaitu :");
+//				for (int i=0 ; i<artikelTerkait.size() ; i++) {
+//		            System.out.println(" - [" + (i+1) + "] " + artikelTerkait.get(i)) ;
+//		        }
+//				Integer artikelIndex = getInt("Pilih artikel yang anda inginkan", 1, artikelTerkait.size()) ;
+//				if (artikelIndex != null){
+//				penampung = _wikipedia.getArticleByTitle(artikelTerkait.get(artikelIndex));
+//				System.out.println(c.getSentenceClean(penampung));
+//				}
+//			}
+	
+}
+
+/*
+ * @param id adalah id dari artikel yang ingin dilihat kategorinya
+ * 
+ */
+
+private void getCategoryParentChild(int id){
+	
+	Page article = _wikipedia.getPageById(id);
+//    System.out.println(article.getType());
+//    System.out.println(article.getTitle());
+    
+    
+    /// WORKSPACE ///
+    
+    Category kategori = _wikipedia.getCategoryByTitle(article.getTitle());
+    
+    if(kategori != null){
+    	
+    	System.out.println(kategori.getId());
+    	System.out.println(kategori.getTitle());
+    }
+    
+   Category[] tempChild  = kategori.getChildCategories();
+   Category[] tempParent = kategori.getParentCategories();
+   
+   if (tempChild.length == 0) {
+	   System.out.println("=== Kategori ini tidak mempunyai turunan kategori ===");
+   } else {
+	   System.out.println("=== Kategori Turunan untuk Kategori ini yaitu : ===");
+       for (int i=0 ; i<tempChild.length ; i++) { 
+           System.out.println(" - [" + (i+1) + "] " + tempChild[i].getTitle()) ;
+       }
+       
+   }
+   
+   System.out.println("\n") ;
+   
+   if (tempParent.length == 0) {
+	   System.out.println("=== Kategori ini tidak mempunyai parent kategori ===");
+   } else {
+	   System.out.println("=== Kategori parent untuk Kategori ini yaitu : ===");
+       for (int i=0 ; i<tempParent.length ; i++) {
+    	   
+           System.out.println(" - [" + (i+1) + "] " + tempParent[i].getTitle()) ;
+       }
+       
+   }
+}
+	
+    public static void main(String args[]) throws Exception {
+    	WikipediaConfiguration conf = new WikipediaConfiguration(new File("X:/Database Berkeley/wikipedia-miner-1.2.0/wikipedia-template.xml"));
+    	Wikicorpus korpus = new Wikicorpus(conf);
+    	//korpus.ArticleSet((new File("X:/Database Berkeley/datadir/pageId.csv")), _wikipedia);
+    	
+    	//korpus.docer();
+    	
+    	//korpus.deskriptor();
+    	
+    	korpus.getArticleByCategory();
+    	
+    	/*
+    	 * membuat corpus dari seluruh artikel wikipedia
+    	 */
+    	//korpus.corpusClean();
+    	
+    }
+
+   
+
+	
+}
